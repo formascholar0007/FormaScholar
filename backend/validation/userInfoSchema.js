712d3fd9da8f397766c73feb04068151edf5c9de
formascholar0007 <formascholar@gmail.com>
2024-03-31 18:19:42 +0530

    fix : userAdditional

--- backend/validation/userInfoSchema.js
+++ backend/validation/userInfoSchema.js
@@ -4,7 +4,7 @@ const userAdditionalSchemaJoi = Joi.object({
     fullName: Joi.string().required().label('Full Name'),
     about: Joi.string().allow('').optional().label('About'),
     image: Joi.string().allow('').optional().label('Image'),
-    phoneNumber: Joi.string().label('Phone Number'),
+    phoneNumber: Joi.string().allow('').optional().label('Phone Number'),
     gender: Joi.string().valid('male', 'female', 'other').required().label('Gender'),
     className: Joi.string().required().label('Class Name')
 });
