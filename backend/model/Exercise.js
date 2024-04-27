21d95ea978a115d7e91893f9de87820f1cd4ad75
formascholar0007 <formascholar@gmail.com>
2024-04-27 20:21:01 +0530

    add : exercise crud

--- backend/model/Exercise.js
+++ backend/model/Exercise.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const ExerciseSchema = mongoose.Schema({
-     exerciseName:{
-        type:String,
-     },
-     chapterId:{
-        type:mongoose.Types.ObjectId,
-        required:true
-     },
-     subjectId:{
-        type:mongoose.Types.ObjectId,
-        required:true
-     },
-     classId:{
-        type:mongoose.Types.ObjectId,
-        required:true
-     }
+   exerciseName: {
+      type: String,
+   },
+   chapterId: {
+      type: mongoose.Types.ObjectId,
+      required: true
+   },
+   subjectId: {
+      type: mongoose.Types.ObjectId,
+      required: true
+   },
+   classId: {
+      type: mongoose.Types.ObjectId,
+      required: true
+   }
 
-},{
-    timestamps:true
+}, {
+   timestamps: true
 });
 
-const ExerciseModel = mongoose.model(ExerciseSchema);
+const ExerciseModel = mongoose.model("Exercise", ExerciseSchema);
 module.exports = ExerciseModel;
--- backend/model/Question.js
+++ backend/model/Question.js
@@ -2,37 +2,37 @@
 const mongoose = require('mongoose');
 
 const QuestionSchema = mongoose.Schema({
-    questionNo : {
-        type:String,
-        required:true
+    questionNo: {
+        type: String,
+        required: true
     },
-    question:{
-        type:String,
-        required:true
+    question: {
+        type: String,
+        required: true
     },
-    answer:{
-        type:String,
-        required:true
+    answer: {
+        type: String,
+        required: true
     },
-    exerciseId:{
-        type:mongoose.Types.ObjectId,
+    exerciseId: {
+        type: mongoose.Types.ObjectId,
     },
-    chapterId:{
-        type:mongoose.Types.ObjectId,
-        required:true
+    chapterId: {
+        type: mongoose.Types.ObjectId,
+        required: true
     },
-    subjectId:{
-        type:mongoose.Types.ObjectId,
-        required:true
+    subjectId: {
+        type: mongoose.Types.ObjectId,
+        required: true
     },
-    classId:{
-        type:mongoose.Types.ObjectId,
-        required:true
+    classId: {
+        type: mongoose.Types.ObjectId,
+        required: true
     }
 
-},{
-    timestamps:true
+}, {
+    timestamps: true
 });
 
-const QuestionModel = mongoose.model(QuestionSchema);
+const QuestionModel = mongoose.model("Question", QuestionSchema);
 module.exports = QuestionModel;
